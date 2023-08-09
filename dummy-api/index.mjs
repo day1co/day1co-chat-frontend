8f61e8bdd6dba9eff30a92e4246f628118b987d4
day1co <dev_adm@day1company.co.kr>
2023-08-09 10:02:06 +0900

    fcfc/dummy: move routes out to subdir

--- dummy-api/index.mjs
+++ dummy-api/index.mjs
@@ -1,9 +1,10 @@
 import http from 'http'
-import fs from 'fs'
 
 import express from 'express'
 import { Server } from 'socket.io'
 import bodyParser from 'body-parser'
+
+import history from './routes/history.mjs'
 
 const app = express()
 const server = http.createServer(app)
@@ -13,49 +14,7 @@ app.use(bodyParser.json())
 
 ///
 
-const history = JSON.parse(fs.readFileSync('./fixture/history.json'))
-
-const searchByContext = (query) =>
-  Object
-    .entries(history)
-    .filter(([ id, record ]) => {
-      for(const k in query)
-        if(k in record.context && record.context[k] != query[k])
-          return false
-      return true
-    }).map(([ id, record ]) =>
-      ({ id, ...record })
-    )
-
-app.get('/history', (req, res) => {
-  const context = req.query
-  const records = searchByContext(context)
-
-  res.json(records)
-})
-
-app.put('/history', (req, res) => {
-  const { question, context } = req.body
-  const duplicated = searchByContext(context).find(entry => question === entry.question)
-
-  if(duplicated) {
-    res.status(409)
-    res.json(duplicated)
-    return
-  }
-
-  const id = 1 + Math.max(...Object.keys(history))
-
-  history[id] = { question, context }
-
-  res.json({ id, ...history[id] })
-})
-
-app.delete('/history/:id', (req, res) => {
-  const { id } = req.params
-  delete history[id]
-  res.json({})
-})
+app.use('/history', history)
 
 ///
 
---
+++ dummy-api/routes/history.mjs
@@ -0,0 +1,53 @@
+import fs from 'fs'
+
+import express from 'express'
+
+const app = express()
+
+const history = JSON.parse(fs.readFileSync('./fixture/history.json'))
+
+const searchByContext = (query) =>
+  Object
+    .entries(history)
+    .filter(([ id, record ]) => {
+      for(const k in query)
+        if(k in record.context && record.context[k] != query[k])
+          return false
+      return true
+    }).map(([ id, record ]) =>
+      ({ id, ...record })
+    )
+
+app.get('/', (req, res) => {
+  const context = req.query
+  const records = searchByContext(context)
+
+  res.json(records)
+})
+
+app.put('/', (req, res) => {
+  const { question, context } = req.body
+  const duplicated = searchByContext(context).find(entry => question === entry.question)
+
+  if(duplicated) {
+    res.status(409)
+    res.json(duplicated)
+    return
+  }
+
+  const id = 1 + Math.max(...Object.keys(history))
+
+  history[id] = { question, context }
+
+  res.json({ id, ...history[id] })
+})
+
+app.delete('/:id', (req, res) => {
+  const { id } = req.params
+  delete history[id]
+  res.json({})
+})
+
+///
+
+export default app
